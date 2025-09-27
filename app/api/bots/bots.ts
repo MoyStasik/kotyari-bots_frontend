41bdb868f5180f90b55d9dbee45d0e158b00bc80
Stanislav Kriger <s.kriger@vkteam.ru>
2025-09-27 19:16:49 +0300

    BOT-26: create api client

---
+++ app/api/bots/bots.ts
@@ -0,0 +1,26 @@
+import { ApiClient } from '../ApiClient';
+import type { GetBotsRequestData, GetBotsResponse } from './bots.types';
+
+class BotsApiClient extends ApiClient {
+  public botsUrl = 'bots';
+  public createBotsUrl = 'bots';
+
+  public async getBots(data: GetBotsRequestData) {
+    const response = await this.get<GetBotsResponse, GetBotsRequestData>({
+      url: this.botsUrl,
+      ...data,
+    });
+
+    return response;
+  }
+
+  public async createBots() {
+    const response = await this.post({
+      url: this.createBotsUrl,
+    });
+
+    return response;
+  }
+}
+
+export const useBotsApiClient = () => new BotsApiClient();
